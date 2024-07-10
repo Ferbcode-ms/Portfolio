0e0d2cefdb1b8788a958025a4a6d85e4c01ad98f
manpreet <manpreet26simmak@gmail.com>
2024-07-10 11:58:25 +0530

    portfolio

--- src/section/MainContent.jsx
+++ src/section/MainContent.jsx
@@ -22,7 +22,7 @@ const MainContent = () => {
   return (
     <div className="relative w-[65%] rounded-[20px] border-[1px] border-solid border-jet bg-eerie-black-2 p-[30px] max-xl:w-full max-md:p-4">
       {/* Navigation */}
-      <nav className="absolute right-0 top-0 z-40 w-max rounded-bl-[20px] rounded-tr-[20px] border-[1px] border-solid border-jet bg-eerie-black-1 px-[8px] shadow-none backdrop-blur-[10px] max-md:fixed max-md:bottom-0 max-md:right-0 max-md:top-auto max-md:w-full max-md:rounded-lg max-md:p-2">
+      <nav className="absolute right-0 top-0 z-40 w-max rounded-bl-[20px] rounded-tr-[20px] border-[1px] border-solid border-jet bg-eerie-black-1 px-[8px] shadow-none backdrop-blur-[10px] max-md:fixed max-md:bottom-0 max-md:right-0 max-md:top-auto max-md:w-full max-md:rounded-lg max-md:p-0">
         <ul className="flex flex-wrap items-center justify-center gap-7 max-lg:gap-1">
           {navItems.map((item) => (
             <li key={item.id} className="bottom-2 font-fw-500 text-light-gray">
